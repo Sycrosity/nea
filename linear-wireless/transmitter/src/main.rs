2be0d4516b19b2dd8a90fe3543bc66b8cdba28d3
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2024-09-16 00:45:11 +0100

    docs: add comments describing the linear-wireless code

--- linear-wireless/transmitter/src/main.rs
+++ linear-wireless/transmitter/src/main.rs
@@ -38,19 +38,6 @@ async fn main(spawner: Spawner) {
 
     let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
 
-    let init = initialize(
-        EspWifiInitFor::Wifi,
-        timg0.timer0,
-        Rng::new(peripherals.RNG),
-        peripherals.RADIO_CLK,
-        &clocks,
-    )
-    .unwrap();
-
-    let wifi = peripherals.WIFI;
-    let esp_now = esp_wifi::esp_now::EspNow::new(&init, wifi).unwrap();
-    info!("esp-now version: {}", esp_now.get_version().unwrap());
-
     cfg_if::cfg_if! {
         if #[cfg(feature = "esp32")] {
             let timg1 = TimerGroup::new(peripherals.TIMG1, &clocks);
@@ -62,35 +49,62 @@ async fn main(spawner: Spawner) {
         }
     }
 
+    // Initialize ESP-WIFI
+    let init = initialize(
+        EspWifiInitFor::Wifi,
+        timg0.timer0,
+        Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+        &clocks,
+    )
+    .unwrap();
+
+    // Define the wifi peripheral
+    let wifi = peripherals.WIFI;
+
+    // Initialize ESP-NOW
+    let esp_now = esp_wifi::esp_now::EspNow::new(&init, wifi).unwrap();
+    info!("esp-now version: {}", esp_now.get_version().unwrap());
+
+    // Split the ESP-NOW peripheral into its components
     let (_, sender, _) = esp_now.split();
 
+    // Create a shared Mutable reference to the sender, allowing multiple buttons to send messages independently
     let sender = mk_static!(
         Mutex::<NoopRawMutex, EspNowSender<'static>>,
         Mutex::<NoopRawMutex, _>::new(sender)
     );
 
+    // Map the button colours to their respective pins
     let buttons = [
         Button::new(AnyInput::new(io.pins.gpio6, Pull::Up), Colour::Red),
         Button::new(AnyInput::new(io.pins.gpio10, Pull::Up), Colour::Yellow),
         Button::new(AnyInput::new(io.pins.gpio5, Pull::Up), Colour::Blue),
     ];
 
+    // Spawn a broadcaster task for each button
     for button in buttons {
         info!("{} button ready.", button.colour);
         spawner.must_spawn(broadcaster(sender, button));
     }
 }
 
+/// Task that listens for button presses and sends a message when a button is pressed.
 #[embassy_executor::task(pool_size = 3)]
 async fn broadcaster(
     sender: &'static Mutex<NoopRawMutex, EspNowSender<'static>>,
     mut button: Button,
 ) {
     loop {
+
         button.pin.wait_for_rising_edge().await;
 
         trace!("{} button pressed", button.colour);
+
+        // Lock the sender mutex to allow the task to send a message
         let mut sender = sender.lock().await;
+
+        // Send the button press as a single byte message
         match sender
             .send_async(&BROADCAST_ADDRESS, &[button.colour.to_u8()])
             .await
